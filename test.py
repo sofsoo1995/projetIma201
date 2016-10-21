9c15a0fef891511c0ff01af158bcf091cfc5ad0c
Soso <sofianehorache@yahoo.fr>
2016-10-21 02:33:55 +0200

    high complexity

--- test.py
+++ test.py
@@ -1,42 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import mahotas as mh
-from random import randint
 
+from random import randint
+import cv2
 
 def D(M1, M2):
-    interm = np.sqrt(sum([sum([(M1[i][j]-M2[i, j])**2 for i in range(len(M1))]) for j in range(len(M1))]))
-    return np.average(interm)  
+    ##calcule de distance entre des patchs
+    interm = np.linalg.norm(M1-M2)
+    return interm  
     
-image_elephant = mh.imread("/home/soso/Documents/cours/telecom/2_annee/ima201/elephant.jpg")
-size_patch = 5
-support = image_elephant.shape[:2]
+image = cv2.imread("TP_C01_001_copy.png", 0)
+size_patch = (5, 5)
+support = image.shape[:2]
 L = 10
-fz = np.array([[[0,0] for i in range(support[1]) ] for i in range(support[0])])
-
-im_field = np.array([[ [0,0,0] for i in range(support[1])] for j in range(support[0])])
+fz = np.zeros((support[0], support[1], 2))
 number_iteration = 2
-patch = np.array([ [ [ [[0,0,0] for i in range(size_patch)] for i in range(size_patch)] for i in range(support[1])] for i in range(support[0])])                                                
+TD1 = 8
 
 #-------------phase initialisation----------------------------------------------------
 for i in range(support[0]):
     for j in range(support[1]):
-        x = randint(0, support[0]-1)
-        y = randint(0, support[1]-1)
-        fz[i][j][0] = x - i
-        fz[i][j][1] = y - j
-        im_field[i][j] = image_elephant[x][y]
-        if ((i > size_patch/2 and i < support[0] - size_patch / 2 - 1) and (j > size_patch / 2 and j < support[1] - size_patch / 2 - 1)):
-            patch[i][j] = image_elephant[i-size_patch//2:i+size_patch//2+1, j-size_patch//2:j+size_patch//2+1]
-            
+        while(np.linalg.norm(fz[i, j], np.inf) < TD1):
+            # But : eloigner suffisamment pour qu'il trouve des images similaires
+            x = randint(0, support[0]-1)
+            y = randint(0, support[1]-1)
+            fz[i][j][0] = x - i
+            fz[i][j][1] = y - j
         
 print("paaaaaatchMatch")
 #-----------algorithme du patchMatch---------------------------------
 fi = []
 for turn in range(number_iteration):
 
-    for i in range(size_patch//2, support[0]-size_patch//2-1):
-        for j in range(size_patch//2, support[1]-size_patch//2-1):
+    for i in range(support[0]):
+        for j in range(support[1]):
             for k in range(L):
                 x = 0
                 y = 0
@@ -44,12 +41,14 @@ for turn in range(number_iteration):
                     x = randint(-1, 1)
                     y = randint(-1, 1)
                     f0 = fz[i][j]+(2**(k))*np.array([x, y])
-                if(i+f0[0] in range(support[0]) and j+f0[1] in range(support[1])):
+                if(i+f0[0] > 0 and i+f0[0]< support[0] and j+f0[1] >0 and j+f0[1]< support[1]):
                     fi.append(f0)
             fi.append(fz[i][j])
-            fz[i, j] = fi[np.argmin([D(patch[i, j], patch[i + f[0], j + f[1]]) for f in fi])]
+            M1 = cv2.getRectSubPix(image, size_patch, (i, j))
+            fz[i,j] = np.argmin([D(M1,cv2.getRectSubPix(image, size_patch, (i+v[0], j+v[1]))) for v in fi])
+            
             fi = []
-    np.save("NNFelephant%s" % turn, fz)
+    np.save("NNFim%s" % turn, fz)
 
 
 
