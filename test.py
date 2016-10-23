3c679ae135d937101522246975f0675c88f2d2de
Soso <sofianehorache@yahoo.fr>
2016-10-23 19:44:42 +0200

    première étape réalisé malgré la compléxité énorme. Il reste le post traitement

--- test.py
+++ test.py
@@ -13,11 +13,11 @@ def D(M1, M2):
     return interm
 
 image = cv2.imread("TP_C02_007_copy.png", 0)
-size_patch = (8, 8)
+size_patch = (15, 15)
 support = image.shape[:2]
 L = 10
 fz = np.zeros((support[0], support[1], 2))
-number_iteration = 2
+number_iteration = 4
 TD1 = 8
 
 # -------------phase initialisation-------------------------
@@ -37,8 +37,8 @@ np.save("NNFrandom", fz)
 
 def updateNNFRandom(fz, s1, s2):
     # permet de trouver le meilleur voisin par une propagation aleatoire
-    M1 = cv2.getRectSubPix(image, size_patch, (s1, s2))
-    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(i+fz[s1][s2][0],j+fz[s1][s2][1])))
+    M1 = cv2.getRectSubPix(image, size_patch, (s2, s1))
+    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(s2+fz[s1][s2][1],s1+fz[s1][s2][0])))
     fmin = fz[s1, s2]
     for k in range(L):
         x = 0
@@ -47,8 +47,8 @@ def updateNNFRandom(fz, s1, s2):
             x = randint(-1, 1)
             y = randint(-1, 1)
         f0 = fz[s1][s2] + (2**(k))*np.array([x, y])
-        if(s1+f0[1] > 0 and s1+f0[1] < support[0] and s2 + f0[0] > 0 and s2+f0[0]<support[1]):
-            M2 = cv2.getRectSubPix(image, size_patch, (s1+f0[1], s2+f0[0]))
+        if(s1+f0[0] > 0 and s1+f0[0] < support[0] and s2 + f0[1] > 0 and s2+f0[1]<support[1]):
+            M2 = cv2.getRectSubPix(image, size_patch, (s2+f0[1], s1+f0[0]))
             if(D(M1, M2) < distMin and np.linalg.norm(f0, np.inf) >= TD1):
                 fmin = f0
                 distMin = D(M1, M2)
@@ -57,16 +57,16 @@ def updateNNFRandom(fz, s1, s2):
 
 def updateNNFProp(fz, s1, s2):
     # permet de construire le NNF iterativement en regardant les precedents
-    M1 = cv2.getRectSubPix(image, size_patch, (s1, s2))
-    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(i+fz[s1][s2][0],j+fz[s1][s2][1])))
+    M1 = cv2.getRectSubPix(image, size_patch, (s2, s1))
+    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(s2+fz[s1][s2][1],s1+fz[s1][s2][0])))
     fmin = fz[s1, s2]
     fi = []
-    if (i > 0):
+    if (s1 > 0):
         fi.append(fz[s1-1, s2])
-    if(j > 0):
+    if(s2 > 0):
         fi.append(fz[s1, s2-1])
     for v in fi:
-        M2 = cv2.getRectSubPix(image, size_patch, (s1 + v[1], s2 + v[0]))
+        M2 = cv2.getRectSubPix(image, size_patch, (s2 + v[1], s1 + v[0]))
         if(D(M1, M2) < distMin):
             fmin = v
             distMin = D(M1, M2)
@@ -77,8 +77,8 @@ start = time.time()
 
 for turn in range(number_iteration):
 
-    for j in range(5, support[1]-5):
-        for i in range(5, support[0]-5):
+    for j in range(0, support[1]-1):
+        for i in range(0, support[0]-1):
             fz[i, j] = updateNNFProp(fz, i, j)
             
             fz[i, j] = updateNNFRandom(fz, i, j)
