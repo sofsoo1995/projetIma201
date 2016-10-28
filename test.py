83397b2a492314a69c27bdbe8e7ef7b450c926a0
Soso <sofianehorache@yahoo.fr>
2016-10-28 22:20:36 +0200

    On a pas baisser de beaucoup le temps

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,87 +5,31 @@ from random import randint
 import cv2
 import time
 
-def D(M1, M2):
-    # calcule de distance entre des patchs
-    interm = np.linalg.norm(M1-M2)
-    return interm
+from patchmatch import *
+# -------------phase initialisation-------------------------
 
 image = cv2.imread("TP_C02_007_copy.png", 0)
 size_patch = (15, 15)
 support = image.shape[:2]
 L = 10
-fz = np.zeros((support[0], support[1], 2))
-number_iteration = 4
+
+number_iteration = 1
 TD1 = 8
 
-# -------------phase initialisation-------------------------
-for i in range(support[0]):
-    for j in range(support[1]):
-        while(np.linalg.norm(fz[i, j], np.inf) < TD1):
-            # But : eloigner suffisamment pour qu'il trouve des images similaires
-            x = randint(0, support[0]-1)
-            y = randint(0, support[1]-1)
-            fz[i][j][0] = x - i
-            fz[i][j][1] = y - j
-        
-print("paaaaaatchMatch")
-# -----------algorithme du patchMatch---------------------------------
-np.save("NNFrandom", fz)
-
-
-def updateNNFRandom(fz, s1, s2):
-    # permet de trouver le meilleur voisin par une propagation aleatoire
-    M1 = cv2.getRectSubPix(image, size_patch, (s2, s1))
-    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(s2+fz[s1][s2][1],s1+fz[s1][s2][0])))
-    fmin = fz[s1, s2]
-    for k in range(L):
-        x = 0
-        y = 0
-        while(x == y == 0):
-            x = randint(-1, 1)
-            y = randint(-1, 1)
-        f0 = fz[s1][s2] + (2**(k))*np.array([x, y])
-        if(s1+f0[0] > 0 and s1+f0[0] < support[0] and s2 + f0[1] > 0 and s2+f0[1]<support[1]):
-            M2 = cv2.getRectSubPix(image, size_patch, (s2+f0[1], s1+f0[0]))
-            if(D(M1, M2) < distMin and np.linalg.norm(f0, np.inf) >= TD1):
-                fmin = f0
-                distMin = D(M1, M2)
-    return fmin
-
-
-def updateNNFProp(fz, s1, s2):
-    # permet de construire le NNF iterativement en regardant les precedents
-    M1 = cv2.getRectSubPix(image, size_patch, (s2, s1))
-    distMin = D(M1,cv2.getRectSubPix(image, size_patch,(s2+fz[s1][s2][1],s1+fz[s1][s2][0])))
-    fmin = fz[s1, s2]
-    fi = []
-    if (s1 > 0):
-        fi.append(fz[s1-1, s2])
-    if(s2 > 0):
-        fi.append(fz[s1, s2-1])
-    for v in fi:
-        M2 = cv2.getRectSubPix(image, size_patch, (s2 + v[1], s1 + v[0]))
-        if(D(M1, M2) < distMin):
-            fmin = v
-            distMin = D(M1, M2)
-    return fmin
-
-
+# -------------execution du code--------------------
+fz = init_nnf(support, TD1)
+print("patchMatch")
 start = time.time()
+k = np.zeros((4, 4))
 
-for turn in range(number_iteration):
+fz = calcul_nnf(number_iteration, support, fz, image, size_patch, L, TD1)
 
-    for j in range(0, support[1]-1):
-        for i in range(0, support[0]-1):
-            fz[i, j] = updateNNFProp(fz, i, j)
-            
-            fz[i, j] = updateNNFRandom(fz, i, j)
-    np.save("NNFim3%s" % turn, fz)
-
-# post-processing--------------------------
 end = time.time()
+
+# affichage du resultat
 print(end-start)
 magn = np.sqrt(fz[:, :, 0]**2 + fz[:, :, 1]**2)
 plt.imshow(magn)
 plt.colorbar()
 plt.show()
+
